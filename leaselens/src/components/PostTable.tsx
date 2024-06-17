152d7e0be21342bca56ce86ceaa6ad0236d02713
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-06-17 14:00:24 +0900

    add: proCard, PostTable

---
+++ leaselens/src/components/PostTable.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "../assets/scss/LJG.scss";
+
+export default function PostTable() {
+  return (
+    <div className="postTable">
+      <table className="postTable_table">
+        <thead className="postTable_thead">
+          <tr className="postTable_head_tr">
+            <th className="postTable_head_th">번호</th>
+            <th className="postTable_head_th">작성일</th>
+            <th className="postTable_head_th">제품명</th>
+            <th className="postTable_head_th">제목</th>
+            <th className="postTable_head_th">작성자</th>
+            <th className="postTable_head_th">인증</th>
+          </tr>
+        </thead>
+        <tbody className="postTable_tbody">
+          <tr className="postTable_body_tr">
+            <th className="postTable_body_th">1</th>
+            <th className="postTable_body_th">2024-06-17</th>
+            <th className="postTable_body_th">TV</th>
+            <th className="postTable_body_th">렌탈 좋네요.</th>
+            <th className="postTable_body_th">아이디</th>
+            <th className="postTable_body_th">
+              <input type="checkbox" name="" id="" checked />
+            </th>
+          </tr>
+        </tbody>
+        <tbody className="postTable_tbody">
+          <tr className="postTable_body_tr">
+            <th className="postTable_body_th">2</th>
+            <th className="postTable_body_th">2024-06-17</th>
+            <th className="postTable_body_th">TV</th>
+            <th className="postTable_body_th">렌탈 안좋네요.</th>
+            <th className="postTable_body_th">아이디</th>
+            <th className="postTable_body_th">
+              <input type="checkbox" name="" id="" disabled/>
+            </th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- leaselens/src/components/proCard.tsx
+++ leaselens/src/components/proCard.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import '../assets/scss/LJG.scss'
+import proImage from "../assets/images/Product/LG OLED 스마트 TV 55인치.png";
 
-export default function proCard() {
+export default function ProCard() {
   return (
-    <div>proCard</div>
+    <div className='proCard'>
+    <section className="proCard_img_container">
+      <img src={proImage} alt="" className="proCard_img" />
+    </section>
+    <section className="proCard_name_section">
+      <p className='proCard_pro_name'>LG OLED 스마트 TV 55인치</p>
+    </section>
+    <section className="proCard_button_section">
+      <button className='proCard_button'>제품 보기</button>
+    </section>
+  </div>
   )
 }
