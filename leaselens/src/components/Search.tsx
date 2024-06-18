5c25de674baf67ad6d503281df40d88e0461340d
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-06-18 12:37:58 +0900

    add: Search

---
+++ leaselens/src/components/Search.tsx
@@ -0,0 +1,27 @@
+import "../assets/scss/LJG.scss";
+
+export default function Search() {
+  return (
+    <div className="search">
+      <div className="search_inputBox">
+        <input
+          type="text"
+          className="search_input"
+          placeholder="제품명을 입력해주세요."
+        />
+        <button className="search_sendBtn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="currentColor"
+            className="bi bi-search"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+}
