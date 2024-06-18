0329c56aaed8594d6f4cf62804d6858c60bd5344
BB545 <black1016@hanmail.net>
2024-06-18 12:15:52 +0900

    add: 사이드 바 카테고리 컴포넌트 구현

--- leaselens/src/components/SideBar.tsx
+++ leaselens/src/components/SideBar.tsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function SideBar() {
   return (
-    <div>
-        
+    <div className='side_container'>
+        <div className='side_all'>전체 상품</div>
+        <div className='side_hr'></div>
+        <ul className='side_category'>
+          <li className='side_item'>냉장고</li>
+          <li className='side_item'>에어컨</li>
+          <li className='side_item'>세탁기</li>
+          <li className='side_item'>TV</li>
+          <li className='side_item'>정수기</li>
+          <li className='side_item'>공기청정기</li>
+          <li className='side_item'>청소기</li>
+        </ul>
     </div>
   )
 }
