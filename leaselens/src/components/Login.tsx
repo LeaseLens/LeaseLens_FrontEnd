12bf050d01dcc4e03a01ec5207477febf76b218c
BB545 <black1016@hanmail.net>
2024-06-18 12:51:07 +0900

    mod: 로그인 컴포넌트 스타일 수정 및 회원 가입 코드 추가

--- leaselens/src/components/Login.tsx
+++ leaselens/src/components/Login.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Login() {
   return (
     <div>
+      {/* login */}
       <form action="" method="post" id="login_form">
         <div id="login_body">
           <div id="login_welcome">
@@ -22,6 +23,36 @@ export default function Login() {
           </div>
           <div id="login_to_signup">
             <a href="#">Sign up</a>
+          </div>
+        </div>
+      </form>
+
+      {/* sign up */}
+      <form action="" method="post" id="login_form">
+        <div id="signup_body">
+          <div id="login_welcome">
+            <div id="login_welcome-1">Lease Lens</div>
+            <div id="login_welcome-2">Welcome to Lease Lens</div>
+          </div>
+          <div id="login_input">
+            <div className="login_input_item">
+              <input placeholder="name" type="text" />
+            </div>
+            <div className="login_input_item">
+              <input placeholder="id" type="text" />
+            </div>
+            <div className="login_input_item">
+              <input placeholder="Password" type="password" />
+            </div>
+            <div className="login_input_item">
+              <input placeholder="Check Password" type="password" />
+            </div>
+          </div>
+          <div id="login_submit">
+            <button id="login_submitBtn" type="submit">Sign Up</button>
+          </div>
+          <div id="login_to_signup">
+            <a href="#">Login</a>
           </div>
         </div>
       </form>
