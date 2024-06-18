627e43bdee1e0482beada6c3f60f282cfa5ac4b5
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-06-18 13:21:12 +0900

    mod: App.tsx, package

--- leaselens/src/App.tsx
+++ leaselens/src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+// import Comment from "./components/Comment";
+// import Search from "./components/Search";
+import PostPage from "./components/PostPage";
 
 function App() {
   return (
     <div className="App">
-      
+      {/* <Comment /> */}
+      {/* <Search /> */}
+      <PostPage />
     </div>
   );
 }
