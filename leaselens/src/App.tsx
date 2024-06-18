fe795232e7c8824edb0d30d8e6f4740cbefc303c
BB545 <black1016@hanmail.net>
2024-06-18 12:16:55 +0900

    add: 로그인 컴포넌트 구현 및 스타일 지정

--- leaselens/src/App.tsx
+++ leaselens/src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Footer from './components/Footer';
 import './assets/scss/hyunh.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import RevCard from './components/RevCard';
+import SideBar from './components/SideBar';
+import Profile from './components/Profile';
+import Banner from './components/Banner';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SideBar />
+      <RevCard />
+      <Profile />
+      <Banner />
+      <Login />
       <Footer />
     </div>
   );
