a6f6f00dcc8c7a8857cc728cd115266cd9510bac
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-06-19 16:29:22 +0900

    mod: App.tsx

--- leaselens/src/App.tsx
+++ leaselens/src/App.tsx
@@ -1,16 +1,3 @@
-import React from 'react';
-import Footer from './components/Footer';
-import './assets/scss/hyunh.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import RevCard from './components/RevCard';
-import SideBar from './components/SideBar';
-import Profile from './components/Profile';
-import Banner from './components/Banner';
-import Login from './components/Login';
-// import ProCard from './components/proCard';
-// import PostTable from './components/PostTable';
-import ProInfo from './components/ProInfo';
 // import Comment from "./components/Comment";
 // import Search from "./components/Search";
 import LodingPage from "./screens/LodingPage";
