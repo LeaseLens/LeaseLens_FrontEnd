f25f66ae03b8f2b0f08ba8077edb03d949f11893
BB545 <black1016@hanmail.net>
2024-06-20 12:00:38 +0900

    err: 깃 오류 수정 중

--- leaselens/src/App.tsx
+++ leaselens/src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './assets/scss/hyunh.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ProductPage from "./screens/ProductPage";
@@ -6,22 +5,15 @@ import ProductDetailPage from './screens/ProductDetailPage';
 import EventPage from './screens/EventPage';
 import MyPage from './screens/MyPage';
 import ReviewPostPage from './screens/ReviewPostPage';
-=======
-import './assets/scss/LJG.scss';
-import './assets/scss/hyunh.scss'
->>>>>>> b20ea1291727ad42e03872bd73302fd872612cf3
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       {/* <ProductPage /> */}
       {/* <ProductDetailPage /> */}
       {/* <EventPage /> */}
       {/* <MyPage /> */}
       <ReviewPostPage />
-=======
->>>>>>> b20ea1291727ad42e03872bd73302fd872612cf3
     </div>
   );
 }
