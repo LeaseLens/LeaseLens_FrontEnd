3ee3bf416b32315e8b3a041407ae129be13f41d3
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-06-20 10:22:53 +0900

    mod: Review page

--- leaselens/src/components/GreenBtn.tsx
+++ leaselens/src/components/GreenBtn.tsx
@@ -1,7 +1,7 @@
 export default function GreenBtn() {
   return (
     <section className="proCard_button_section greenBtn">
-      <button className='proCard_button greenBtn_button'>제품 보기</button>
+      <button className='proCard_button greenBtn_button'>글쓰기</button>
     </section>
   )
 }
--- leaselens/src/screens/ReviewPage.tsx
+++ leaselens/src/screens/ReviewPage.tsx
@@ -4,33 +4,29 @@ import GreenBtn from "../components/GreenBtn";
 import Header from "../components/Header";
 import PostTable from "../components/PostTable";
 import Search from "../components/Search";
-import SideBar from "../components/SideBar";
+
+// 임시 어드민 / 일반 사용자 구분
+const adminKey = false;
 
 export default function ReviewPage() {
   return (
     <>
       <Header />
       <main className="revPage">
-        <div className="revPage_sideBar">
-          <div className="propg_side_container">
-            <Search searchOpt={"제품명을 입력해주세요."}/>
-            <SideBar />
-          </div>
-        </div>
         <section className="revPage_main">
           <div className="revPage_banner">
             <Banner bannerTxt={"리뷰 커뮤니티"} />
           </div>
           <div className="revPage_table">
-            <div className="revPage_table_header">
+            {adminKey ? '' : <div className="revPage_table_header">
               <div>
                 <Search searchOpt={"검색할 제목을 입력하세요."}/>
               </div>
               <div>
                 <GreenBtn />
               </div>
-            </div>
-            <PostTable />
+            </div>}
+            <PostTable isAdmin = {adminKey}/>
           </div>
         </section>
       </main>
