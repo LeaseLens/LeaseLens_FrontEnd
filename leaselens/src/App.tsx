9c5f5fba67912d24f46b3d216ef2059c8b5238fd
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-06-21 12:50:14 +0900

    scss merge

--- leaselens/src/App.tsx
+++ leaselens/src/App.tsx
@@ -1,17 +1,9 @@
 import './assets/scss/LJG.scss';
 import './assets/scss/hyunh.scss'
-import MobileSideBar from './components/MobileSideBar';
-import MainPage from "./screens/MainPage";
-import ProductPage from './screens/ProductPage';
-// import ReviewPage from './screens/ReviewPage';
 
 function App() {
   return (
     <div className="App">
-      {/* <MainPage /> */}
-      {/* <ReviewPage /> */}
-      {/* <MobileSideBar /> */}
-      <ProductPage />
     </div>
   );
 }
