3475b5377dacca9053cc172658bc264db88a40eb
BB545 <black1016@hanmail.net>
2024-06-23 18:01:01 +0900

    err: 깃 충돌 오류 해결 중

--- leaselens/src/App.tsx
+++ leaselens/src/App.tsx
@@ -1,12 +1,11 @@
 import './assets/scss/LJG.scss';
 import './assets/scss/hyunh.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MainPage from './screens/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
+
     </div>
   );
 }
