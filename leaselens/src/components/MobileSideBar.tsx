7e6b3dc47fe8fd5da55ab8a401ac7baf310b4f47
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-06-26 10:42:49 +0900

    com: mobileSideBar, productPage

--- leaselens/src/components/MobileSideBar.tsx
+++ leaselens/src/components/MobileSideBar.tsx
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import { useEffect, useState } from 'react'
+import { SidebarProps } from '../types/types';
 
-const MobileSideBar: React.FC = () => {
+export default function MobileSideBar({ getProds }: SidebarProps) {
   const [selectedItem, setSelectedItem] = useState<number | null>(null);
+  const [category, setCategory] = useState(String);
 
   const handleItemClick = (index: number) => {
     setSelectedItem(index);
   };
+
+  useEffect(() => {
+    getProds(category);
+  }, [category]);
 
   const items: string[] = [
     "냉장고",
@@ -29,7 +35,9 @@ const MobileSideBar: React.FC = () => {
               className={`side_item ${
                 selectedItem === index ? "selected" : ""
               }`}
-              onClick={() => handleItemClick(index)}
+              onClick={() => {handleItemClick(index)
+                setCategory(`?category=${item}`)
+              }}
             >
               {item}
             </li>
@@ -39,5 +47,3 @@ const MobileSideBar: React.FC = () => {
     </div>
   );
 };
-
-export default MobileSideBar;
