153bab0bae6e6bd5c0c62e163925d150cddb493c
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-06-28 14:00:14 +0900

    com: myPage review delete

--- leaselens/src/components/PostTable.tsx
+++ leaselens/src/components/PostTable.tsx
@@ -1,8 +1,24 @@
 import { Link } from "react-router-dom";
 import "../assets/scss/LJG.scss";
 import type { PostTableProps } from "../types/types";
+import { Button } from 'react-bootstrap';
+import axios from "axios";
 
-export default function PostTable({ fontSize, thTxt, reviewArr }: PostTableProps) {
+export default function PostTable({
+  fontSize,
+  thTxt,
+  reviewArr,
+}: PostTableProps) {
+  function deleteReview(rev_idx: number | undefined) {
+    if(rev_idx !== undefined) {
+      axios.delete(`http://localhost:8080/reviews/${rev_idx}`);
+      window.location.reload();
+    }
+    else {
+      alert("존재하지 않는 리뷰 입니다.");
+    }
+  }
+
   return (
     <div className="postTable">
       <table className="postTable_table">
@@ -17,19 +33,38 @@ export default function PostTable({ fontSize, thTxt, reviewArr }: PostTableProps
           </tr>
         </thead>
         <tbody className="postTable_tbody">
-
-          { reviewArr && reviewArr.map((review) => {
-            return (
-              <tr className="postTable_body_tr" style={{ fontSize }}>
-                <th className="postTable_body_th">{review.rev_idx}</th>
-                <th className="postTable_body_th">{review.rev_createdAt?.split("T")[0]}</th>
-                <th className="postTable_body_th">{review.Product?.prod_name}</th>
-                <th className="postTable_body_th"><Link to={`/reviews/${review.rev_idx}`}>{review.rev_title}</Link></th>
-                <th className="postTable_body_th">{review.User?.user_ID}</th>
-                <th className="postTable_body_th">{review.rev_isAuth ? <input type="checkbox" checked={true} readOnly={true} className="postTable_body_input"/> : <input type="checkbox" checked={false} readOnly={true} className="postTable_body_input"/>}</th>
-              </tr>
-            )
-          })}
+          {reviewArr &&
+            reviewArr.map((review) => {
+              return (
+                <tr className="postTable_body_tr" style={{ fontSize }}>
+                  <th className="postTable_body_th">{review.rev_idx}</th>
+                  <th className="postTable_body_th">
+                    {review.rev_createdAt?.split("T")[0]}
+                  </th>
+                  <th className="postTable_body_th">
+                    {review.Product?.prod_name}
+                  </th>
+                  <th className="postTable_body_th">
+                    <Link to={`/reviews/${review.rev_idx}`}>
+                      {review.rev_title}
+                    </Link>
+                  </th>
+                  <th className="postTable_body_th">{review.User?.user_ID}</th>
+                  <th className="postTable_body_th">
+                    {window.location.pathname === "/mypage" ? (
+                      <Button variant="outline-danger" className="postDel" onClick={() => {deleteReview(review.rev_idx)}}>삭제</Button>
+                    ) : (
+                      <input
+                        type="checkbox"
+                        checked={review.rev_isAuth}
+                        readOnly
+                        className="postTable_body_input"
+                      />
+                    )}
+                  </th>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
     </div>
